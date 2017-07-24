5fc6d2ecc4f2b81bc30385a496438aed4d8ace30
dmrib <dmrib.cs@gmail.com>
2017-07-24 14:19:33 -0300

    Created landing page

--- termitolib/termitolib/settings/base.py
+++ termitolib/termitolib/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'library',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'termitolib.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
