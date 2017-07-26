56ffc67b9bdcfc1b2eb2363cc85f1d7e2f040c7b
dmrib <dmrib.cs@gmail.com>
2017-07-26 17:58:50 -0300

    Wildcard book search

--- termitolib/books/urls.py
+++ termitolib/books/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from .views import BookCreateView
+from .views import BookCreateView, BookSearchListView
 
 
 urlpatterns = [
                 url(r'^register/$', BookCreateView.as_view(), name='register'),
-                url(r'^search$', TemplateView.as_view(template_name='books/book_search.html'), name='search')
+                url(r'^search$', TemplateView.as_view(template_name='books/book_search.html'), name='search'),
+                url(r'^search_result/$', BookSearchListView.as_view(), name='search-result'),
 ]
--- termitolib/books/views.py
+++ termitolib/books/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from django.views.generic.edit import ProcessFormView
 
 from .forms import BookForm
 from .models import Book
+
 
 class BookCreateView(LoginRequiredMixin, CreateView):
     template_name = 'books/book_register.html'
@@ -15,3 +18,19 @@ class BookCreateView(LoginRequiredMixin, CreateView):
         book = form.save(commit=False)
         book.registered_by = self.request.user
         return super(BookCreateView, self).form_valid(form)
+
+class BookSearchListView(ListView):
+    template_name = 'books/book_search_result.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        term = self.request.GET.get('term')
+        result = Book.objects.filter(Q(name__icontains=term) |
+                                     Q(authors__icontains=term) |
+                                     Q(tags__icontains=term) |
+                                     Q(publisher__icontains=term) |
+                                     Q(isbn__icontains=term) |
+                                     Q(shelf__icontains=term) |
+                                     Q(code__icontains=term)
+                 )
+        return result
