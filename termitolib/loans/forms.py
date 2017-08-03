12fb1a1bc5583b28918db5bf38f30eedacb18fdc
dmrib <dmrib.cs@gmail.com>
2017-08-03 17:36:00 +0000

    Show delayed loans

--- termitolib/loans/forms.py
+++ termitolib/loans/forms.py
@@ -6,12 +6,12 @@ from books.models import Book
 class LoanForm(forms.ModelForm):
     code = forms.CharField(label='Book Code')
 
+    def clean_code(self):
+         if not Book.objects.filter(code = self.cleaned_data.get('code')).exists():
+            raise forms.ValidationError("Not a valid book code")
+
     class Meta:
         model = Loan
         fields = [
                     'to',
         ]
-
-    def clean_code(self):
-        if not Book.objects.filter(code = self.cleaned_data.get('code')).exists():
-            raise forms.ValidationError("Not a valid book code")
--- termitolib/loans/models.py
+++ termitolib/loans/models.py
@@ -1,5 +1,5 @@
 import datetime
-
+from django.utils import timezone
 from django.db import models
 from django.conf import settings
 
@@ -8,7 +8,7 @@ from books.models import Book
 def get_deadline():
     return datetime.datetime.today() + datetime.timedelta(days=7)
 
-    
+
 class Loan(models.Model):
     by = models.ForeignKey(settings.AUTH_USER_MODEL)
     to = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='to')
@@ -22,6 +22,13 @@ class Loan(models.Model):
 
     def __str__(self):
         return (self.item.name + ' - ' +self.to.username)
+
+    @property
+    def is_late(self):
+        if self.return_date < timezone.now():
+            return True
+        else:
+            return False
 
     def get_absolute_url(self):
         return f"/loans/{self.pk}"
