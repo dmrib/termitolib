5083285a6b98d665398edf5b686a99fa9c9ac938
dmrib <dmrib.cs@gmail.com>
2018-07-17 09:39:38 -0300

    data backup

--- termitolib/loans/forms.py
+++ termitolib/loans/forms.py
@@ -6,10 +6,6 @@ from books.models import Book
 class LoanForm(forms.ModelForm):
     code = forms.CharField(label='Book Code')
 
-    def clean_code(self):
-         if not Book.objects.filter(code = self.cleaned_data.get('code')).exists():
-            raise forms.ValidationError("Not a valid book code")
-
     class Meta:
         model = Loan
         fields = [
